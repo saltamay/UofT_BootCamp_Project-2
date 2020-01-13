a736c7bbb8bbf6dc9c70fc1b3fa19cdac1a1d2ac
Sal Tamay <sal.tamay@outlook.com>
2020-01-12 23:54:22 -0500

    Initial project setup

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const routes = require('./routes/api-routes');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static('public'));
+app.use(express.static('public/images'));
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use('/', routes);
+
+app.listen(PORT, () => {
+  console.log(`App now listening at localhost: + ${PORT}`);
+});
