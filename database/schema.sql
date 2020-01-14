d42c0ebd0ea502a5028ee7b58529e276d9ad930c
Anas Qazi <ianasqazi@gmail.com>
2020-01-14 11:36:42 -0500

    Created airport details sql and json for seeding data

--- database/schema.sql
+++ database/schema.sql
@@ -1,2 +1,17 @@
-DROP DATABASE IF EXISTS `sample_db`;
-CREATE DATABASE `sample_db`;
+
+/* CREATE TABLE */
+CREATE TABLE airport_details
+( airport_id int NOT NULL auto_increment,
+  name varchar(100) NOT NULL,
+  city varchar(50) NOT NULL,
+  country varchar(30) NOT NULL,
+  IATA varchar(3),
+  ICAO varchar(4),
+  latitude float(20),
+  longitude float(20),
+  altitude int(50),
+  timezone int(20),
+  daylight_savings_time varchar(5),
+  tz varchar(50),
+  primary key(airport_id)
+);
