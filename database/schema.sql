28f41be371a4298a9969f8cc928bc659894af348
Anas Qazi <ianasqazi@gmail.com>
2020-01-15 13:40:22 -0500

    Updated to create new tables

--- database/schema.sql
+++ database/schema.sql
@@ -21,3 +21,29 @@ CREATE TABLE airport_details
   primary key(airport_id)
 );
 
+CREATE TABLE users_details
+( 
+	id int NOT NULL auto_increment,
+	first_name varchar(30) NOT NULL,
+	last_name varchar(30) NOT NULL,
+    birthdate date,
+    email varchar(50),
+    relationship_status varchar(30),
+    height int(10),
+    hair_colour varchar(50),
+    tagline varchar(100),
+    bio varchar(300),
+    image_url varchar(100),
+	primary key(id)
+);
+
+CREATE TABLE trip_details
+( 
+	id int NOT NULL auto_increment,
+    user_id int NOT NULL,
+    airport varchar(100),
+    time varchar(100),
+	primary key(trip_id),
+	CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES user_details(id)
+);
+
