3d74b677f3c5692d5911ce455be9fa49f9eae736
Sal Tamay <sal.tamay@outlook.com>
2020-01-14 20:00:53 -0500

    bug: Fix eslint errors

--- config/connection.js
+++ config/connection.js
@@ -3,34 +3,34 @@
 // *********************************************************************************
 
 // Require mysql
-var mysql2 = require("mysql2");
+const mysql2 = require('mysql2');
 
 // Set up our connection information
 
-var connection;
+let connection;
 
-if(process.env.JAWSDB_URL){
-    connection = mysql2.createConnection(process.env.JAWSDB_URL);
+if (process.env.JAWSDB_URL) {
+  connection = mysql2.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql2.createConnection({
-        port: "3306",
-        host: "nba02whlntki5w2p.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-        user: "tc73jbdiqqesrb5w",
-        password: "eaysnxddxs48abp8",
-        database: "otr85dh0ggpmbqf6"
-    });
-};
+  connection = mysql2.createConnection({
+    port: '3306',
+    host: 'nba02whlntki5w2p.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'tc73jbdiqqesrb5w',
+    password: 'eaysnxddxs48abp8',
+    database: 'otr85dh0ggpmbqf6'
+  });
+}
 
 // connection.connect();
 
 
 // Connect to the database
-connection.connect(function(err) {
+connection.connect(err => {
   if (err) {
-    console.error("error connecting: " + err.stack);
+    console.error(`error connecting: ${err.stack}`);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log(`connected as id ${connection.threadId}`);
 });
 
 // Export connection
