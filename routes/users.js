efcd268f5803adc0a4920f69bd3ed50dbac5fa06
Sal Tamay <sal.tamay@outlook.com>
2020-01-20 14:22:53 -0500

    feat: Add users and trips routes

---
+++ routes/users.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+
+const {
+  getAllUsers,
+  getSingleUser,
+  searchUsers,
+  createUser,
+  deleteUser,
+  updateUser
+} = require('../controllers/users');
+
+router.get('/', getAllUsers);
+
+router.get('/:id', getSingleUser);
+
+router.get('/search', searchUsers);
+
+router.post('/', createUser);
+
+router.put('/:id', updateUser);
+
+router.delete('/:id', deleteUser);
+
+module.exports = router;
