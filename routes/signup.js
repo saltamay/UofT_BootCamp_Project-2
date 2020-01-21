6ef76a69a987c863f5ecc2852b3ac50c0f169352
Anas Qazi <ianasqazi@gmail.com>
2020-01-20 20:26:23 -0500

    adding post router

--- routes/signup.js
+++ routes/signup.js
@@ -1,7 +1,25 @@
 const router = require('express').Router();
 
+const {
+  getAllUsers,
+  getSingleUser,
+  searchUsers,
+  createUser,
+  updateUser,
+  deleteUser
+} = require('../controllers/users.js')
+
+
+
 router.get('/', (req, res) => {
   res.render('signup');
 });
+
+router.post('/signup', createUser, 
+(req, res) => {
+  console.log("User Added");
+  res.redirect('/');
+});
+
 
 module.exports = router;
